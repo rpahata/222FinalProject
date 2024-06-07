315d874572c6d236b365a8fee28abaa6d6f70a5a
Htet Wai <supernovatomars2025@gmail.com>
2024-06-07 20:06:54 +0700

    Outline

---
+++ src/jobPortal/EmployerMenu.java
@@ -0,0 +1,5 @@
+package jobPortal;
+
+public class EmployerMenu {
+
+}
---
+++ src/jobPortal/Signup.java
@@ -0,0 +1,7 @@
+package jobPortal;
+
+public class Signup {
+	public static void main(String[] args) {
+		System.out.println("Welcome to Job Portal \n Type 1 to login 2 to sign up ");
+	}
+}
